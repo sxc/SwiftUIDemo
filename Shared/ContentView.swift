71865092f11c21bfdae879b67446f6b57627c0f3
sxc <qiuyequ@icloud.com>
2020-12-23 10:59:04 +0800

    add color picker

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    var colors: [Color] = [.black, .red, .green, .blue]
+    var colornames = ["Black", "Red", "Green", "Blue"]
+    
+    @State private var colorIndex = 0
+    
+    @State private var rotation: Double = 0
+    @State private var text: String = "Welcome to SwiftUI"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(text)
+            Text("Hello, world!")
+                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                .fontWeight(.heavy)
+                .rotationEffect(.degrees(rotation))
+                .animation(.easeInOut(duration: 5))
+
+            Slider(value: $rotation, in: 0 ... 360, step: 0.1)
+            TextField("Enter text here", text: $text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Picker(selection: $colorIndex, label: Text("Color")) {
+                ForEach (0 ..< colornames.count) { color in
+                    Text(colornames[color])
+                        .foregroundColor(colors[color])
+                    }
+                }
+        }
+           
     }
 }
 
